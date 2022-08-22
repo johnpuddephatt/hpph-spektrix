dc8e69e5ba2a9b7e9b470445e1d758374bbc3918
John Puddephatt <john@jdp.org.uk>
2022-08-22 19:55:56 +0100

    remove minutely schedule debugger

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,12 +20,6 @@ class Kernel extends ConsoleKernel
             ->command("media-library:delete-old-temporary-uploads")
             ->daily();
 
-        $schedule
-            ->call(function () {
-                logger("every minute!");
-            })
-            ->everyMinute();
-
         $schedule->command("fetch:all")->hourly();
     }
 
