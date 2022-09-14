7f7a77d90d153662d20d2fd41d75e9ee352ff3d6
John Puddephatt <john@jdp.org.uk>
2022-09-14 23:43:18 +0100

    fix image component

--- resources/views/components/home-carousel.blade.php
+++ resources/views/components/home-carousel.blade.php
@@ -11,7 +11,7 @@
          <div class="images">
              @foreach ($page->content->values_images as $image)
                  <x-image :src="$image->src" :srcset="$image->srcset"
-                     class="absolute left-0 block w-[66.667vw] origin-center rounded-3xl lg:w-[50vw] xl:w-[33.3vw]" />
+                     class="absolute left-0 block w-[66.667vw] origin-center rounded-[10em] lg:w-[50vw] xl:w-[33.3vw]" />
              @endforeach
          </div>
      </div>
