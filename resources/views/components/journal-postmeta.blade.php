ea0fca1d60af6b6ce7e57e85ce6ddb176d01522b
John Puddephatt <john@jdp.org.uk>
2023-03-17 20:42:24 +0000

    check we have tags

--- resources/views/components/journal-postmeta.blade.php
+++ resources/views/components/journal-postmeta.blade.php
@@ -5,11 +5,13 @@
         <span class="{{ $dark ? 'text-white' : 'text-dark' }}"> {{ $post->date }}</span>
     </div>
 
-    <div class="gap-1 flex flex-row items-center">
-        @svg('bookmark', 'h-3 w-3 inline-block mr-0.5')
-        @foreach ($post->tags_translated ?? [] as $tag)
-            <span
-                class="type-xs-mono {{ $dark ? 'bg-gray-dark text-white' : 'bg-sand text-black' }} rounded px-1 py-0.5">{{ $tag->name_translated }}</span>
-        @endforeach
-    </div>
+    @if ($post->tags_translated)
+        <div class="gap-1 flex flex-row items-center">
+            @svg('bookmark', 'h-3 w-3 inline-block mr-0.5')
+            @foreach ($post->tags_translated ?? [] as $tag)
+                <span
+                    class="type-xs-mono {{ $dark ? 'bg-gray-dark text-white' : 'bg-sand text-black' }} rounded px-1 py-0.5">{{ $tag->name_translated }}</span>
+            @endforeach
+        </div>
+    @endif
 </div>
