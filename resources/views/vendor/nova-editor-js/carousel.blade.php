f97397dd06f321b3890145a0616a1dbb858dfbdc
John Puddephatt <john@jdp.org.uk>
2023-03-17 21:15:01 +0000

    carousel

--- resources/views/vendor/nova-editor-js/carousel.blade.php
+++ resources/views/vendor/nova-editor-js/carousel.blade.php
@@ -2,9 +2,10 @@
     <div
         class="ml-[calc((100vw-100%)/-2)] flex w-screen flex-row gap-5 overflow-x-auto px-[calc((100vw-100%)/2)] scrollbar-hide">
         @foreach ($images as $image)
-            <figure class="flex-none">
+            <figure
+                class="@if (count($images) == 1) w-full @elseif (count($images) == 2) w-1/2 h-auto @endif flex-none">
                 <img src="{{ $image['url'] }}"
-                    class="@if (count($images) == 1) w-full @elseif (count($images) == 2) w-1/2 h-auto @else h-[32em] w-auto @endif block rounded">
+                    class="@if (count($images) > 2) h-[32em] w-auto @endif block rounded">
 
                 <figcaption class="type-xs-mono py-3">
                     @if (count($images) > 1)
