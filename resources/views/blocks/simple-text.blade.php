f196c1230e3050774dc8c293ddaeaddd1dbb83a8
John Puddephatt <john@jdp.org.uk>
2023-03-23 23:24:25 +0000

    simple text margin

--- resources/views/blocks/simple-text.blade.php
+++ resources/views/blocks/simple-text.blade.php
@@ -1,1 +1,1 @@
-<x-editorjs class="prose py-16 max-w-lg" :content="$layout->section_content" block_class="" />
+<x-editorjs class="prose my-12 max-w-lg" :content="$layout->section_content" block_class="" />
