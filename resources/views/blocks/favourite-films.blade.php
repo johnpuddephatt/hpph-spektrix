0b27db52320cc7f0bed11a8232cf1e577381e632
John Puddephatt <john@jdp.org.uk>
2023-03-23 23:36:48 +0000

    convert obj to array

--- resources/views/blocks/favourite-films.blade.php
+++ resources/views/blocks/favourite-films.blade.php
@@ -1,8 +1,9 @@
 @if ($layout->favourite_films)
-    {{-- <h3 class="type-xs-mono">My top
-        {{ count($layout->favourite_films) > 1 ? count($layout->favourite_films) . ' films' : 'film' }} </h3> --}}
+    @php $layout->favourite_films = (array) $layout->favourite_films @endphp
+    <h3 class="type-xs-mono">My top
+        {{ count($layout->favourite_films) > 1 ? count($layout->favourite_films) . ' films' : 'film' }} </h3>
     <table class="mt-4 mb-12 border-t border-sand-dark w-full max-w-[55ch] table-auto">
-        @foreach ($layout->favourite_films as $year => $film)
+        @foreach ($layout->favourite_films as $film => $year)
             <tr class="border-b border-sand-dark">
                 <td class="py-4 font-bold">{{ $film }}</td>
                 <td class="py-4">{{ $year }}</td>
