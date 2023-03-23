f667837e59a5748bd43afed26a5d078f6c0ccfba
John Puddephatt <john@jdp.org.uk>
2023-03-23 15:28:57 +0000

    fix quote block

--- resources/views/blocks/quote.blade.php
+++ resources/views/blocks/quote.blade.php
@@ -1,6 +1,6 @@
     <div class="py-16 bg-black flex flex-col lg:flex-row">
-        <div class="bg-yellow" style="background-color: @yield('color')"
-            class="flex items-center text-center flex-col justify-center bg-black lg:w-1/2">
+        <div style="background-color: @yield('color')"
+            class="flex items-center text-center flex-col justify-center bg-yellow lg:w-1/2">
             <h2 class="type-medium max-w-lg mx-auto mb-6 lg:mb-16">“{{ $layout->quote }}”</h2>
             <p class="type-xs-mono mx-auto max-w-md">{{ $layout->name }}</p>
             <p class="type-xs-mono mx-auto max-w-md">{{ $layout->role }}</p>
