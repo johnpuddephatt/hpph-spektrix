1e1d62fa1239980cb0e98bb3b08c856d20ab954f
John Puddephatt <john@jdp.org.uk>
2023-03-23 23:34:08 +0000

    loop object?

--- resources/views/blocks/favourite-films.blade.php
+++ resources/views/blocks/favourite-films.blade.php
@@ -1,7 +1,6 @@
 @if ($layout->favourite_films)
-    @dd($layout->favourite_films)
-    <h3 class="type-xs-mono">My top
-        {{ count($layout->favourite_films) > 1 ? count($layout->favourite_films) . ' films' : 'film' }} </h3>
+    {{-- <h3 class="type-xs-mono">My top
+        {{ count($layout->favourite_films) > 1 ? count($layout->favourite_films) . ' films' : 'film' }} </h3> --}}
     <table class="mt-4 mb-12 border-t border-sand-dark w-full max-w-[55ch] table-auto">
         @foreach ($layout->favourite_films as $year => $film)
             <tr class="border-b border-sand-dark">
