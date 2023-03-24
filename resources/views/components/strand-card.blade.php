0d277634ccbc52041daf7baaf0eec40a5e27742a
John Puddephatt <john@jdp.org.uk>
2023-03-24 16:35:11 +0000

    link on strand card

--- resources/views/components/strand-card.blade.php
+++ resources/views/components/strand-card.blade.php
@@ -1,7 +1,8 @@
 @if ($strand)
     <div class="container bg-black pt-12">
         <div class="bg-black-light mx-auto flex flex-col lg:flex-row items-center text-white rounded overflow-hidden">
-            <div class="bg-black relative my-0 lg:w-1/3 bg-opacity-50 self-stretch">
+            <a href="{{ route('strand.show', $strand->slug) }}"
+                class="bg-black relative my-0 lg:w-1/3 bg-opacity-50 self-stretch">
 
                 @if ($strand->featuredImage)
                     {!! $strand->featuredImage->img('landscape')->attributes(['class' => 'w-full h-auto opacity-60']) !!}
@@ -14,7 +15,7 @@
                         class="type-regular lg:group-hover:opacity-0 lg:group-hover:-translate-y-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 mx-auto w-72 max-w-full px-8">
                         {{ $strand->name }}</h3>
                 @endif
-            </div>
+            </a>
             <div class="relative p-8 lg:pr-0 lg:py-4 lg:w-2/3 lg:pl-[16.67%]"">
 
                 <h3
@@ -23,7 +24,7 @@
                 <h3 class="type-regular lg:type-medium lg:mb-8 lg:!font-normal">Part of <span class="!font-bold"
                         style="color: {{ $strand->color }}">{{ $strand->name }}</span></h3>
                 <div class="type-regular max-w-lg !font-normal mb-16 lg:mb-8">{{ $strand->description }}</div>
-                <a class="type-xs-mono" style="color: {{ $strand->color }}"
+                <a class="type-xs-mono before:inset-0" style="color: {{ $strand->color }}"
                     href="{{ route('strand.show', $strand->slug) }}">+
                     More from this strand</a>
 
