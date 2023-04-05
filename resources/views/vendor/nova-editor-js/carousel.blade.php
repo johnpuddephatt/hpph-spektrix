680ef0bcdd88bc7685ced15415a7c5d11202e2ed
John Puddephatt <john@jdp.org.uk>
2023-04-05 21:35:00 +0100

    figcaption

--- resources/views/vendor/nova-editor-js/carousel.blade.php
+++ resources/views/vendor/nova-editor-js/carousel.blade.php
@@ -1,4 +1,4 @@
-<div class="{{ $width != 'full' ? '-mx-6' : '' }} overflow-hidden my-16 bg-sand-light px-4 lg:px-6 pt-6 pb-0">
+<div class="{{ $width != 'full' ? '-mx-6' : '' }} overflow-hidden my-16 bg-sand-light px-4 lg:px-6 pt-6">
     <div
         class="@if (count($images) < 3) lg:flex-row gap-4 lg:gap-5 flex-col @else flex-row gap-5 @endif ml-[calc((100vw-100%)/-2)] flex w-screen overflow-x-auto px-[calc((100vw-100%)/2)] scrollbar-hide">
         @foreach ($images as $image)
@@ -9,7 +9,7 @@
                     class="@if (count($images) > 2) h-[24rem] lg:h-[32em] w-auto @elseif(count($images) == 2) w-full h-auto lg:max-w-[80vw] lg:h-[calc(100%-3rem)] object-cover @else w-full h-auto @endif block rounded">
 
                 @if ($image['caption'])
-                    <figcaption class="type-xs-mono pb-6 lg:pb-3 py-3">
+                    <figcaption class="type-xs-mono pt-2">
 
                         {{ $image['caption'] }}
 
