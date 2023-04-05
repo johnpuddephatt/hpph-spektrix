09294ca1e91e6ecd52ba57cdeb70377f4b834262
John Puddephatt <john@jdp.org.uk>
2023-04-05 23:28:28 +0100

    try scrolling screenings in booking path

--- resources/views/spektrix-components/booking-path.blade.php
+++ resources/views/spektrix-components/booking-path.blade.php
@@ -44,14 +44,14 @@
             <span>Showtimes</span> <span
                 class="type-regular lg:type-medium lg:order-last lg:rotate-90 order-first lg:ml-4 inline-block w-12 h-12 py-3.5 lg:w-16 lg:h-16 lg:py-5 !leading-none rounded-full bg-yellow align-middle text-center">1</span>
         </h2>
-        <div class="container relative py-12 lg:pl-48" x-show="eventID">
+        <div class="container h-screen flex flex-col relative py-12 lg:pl-48" x-show="eventID">
 
             <div class="type-medium !font-normal">Select a showtime</div>
 
             <div x-show="!instances.length" x-transition class="absolute inset-0 bg-sand py-16 pl-32">
                 @svg('loading', 'w-32 ml-36 block pt-24 text-sand-dark')
             </div>
-            <div class="flex flex-col lg:flex-row gap-24" x-show="instances.length">
+            <div class="flex-1 overflow-y-auto flex flex-col lg:flex-row gap-24" x-show="instances.length">
                 <div class="max-w-lg w-full">
                     <div class="mt-1"><span class="font-bold" x-text="event"></span> <span
                             class="type-xs-mono bg-gray-dark inline-block min-w-[2em] text-center rounded-full align-middle px-1 text-white"
