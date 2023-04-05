180a0ef3ffd6cac190548330134dd3454158a3ff
John Puddephatt <john@jdp.org.uk>
2023-04-05 23:42:37 +0100

    pr

--- resources/views/spektrix-components/booking-path.blade.php
+++ resources/views/spektrix-components/booking-path.blade.php
@@ -52,12 +52,12 @@
                 @svg('loading', 'w-32 ml-36 block pt-24 text-sand-dark')
             </div>
             <div class="flex-1 flex flex-col lg:flex-row gap-12 overflow-hidden" x-show="instances.length">
-                <div class="max-w-xl pr-12 w-full flex flex-col h-full">
+                <div class="max-w-xl w-full flex flex-col h-full">
                     <div class="mt-1"><span class="font-bold" x-text="event"></span> <span
                             class="type-xs-mono bg-gray-dark inline-block min-w-[2em] text-center rounded-full align-middle px-1 text-white"
                             x-text="certificate"></span></div>
 
-                    <div class="mt-8 flex-1 overflow-y-auto">
+                    <div class="mt-8 pr-12 flex-1 overflow-y-auto">
                         <template x-for="(instance, key) in instances">
                             <div>
                                 <h3 x-show="key == 0 ||
