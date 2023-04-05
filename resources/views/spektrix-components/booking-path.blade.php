66b0c5f69681b0ec19d7c9a73de57e02da080b66
John Puddephatt <john@jdp.org.uk>
2023-04-05 12:11:45 +0100

    use short_id in booking path

--- resources/views/spektrix-components/booking-path.blade.php
+++ resources/views/spektrix-components/booking-path.blade.php
@@ -63,7 +63,7 @@
                             instances[key - 1].start_date !== instance.start_date"
                                 class="type-small mt-20 mb-3" x-text="instance.start_date"></h3>
                             <button aria-label="Buy tickets for this screening"
-                                x-on:click="instanceID = instance.id.substr(0,5)"
+                                x-on:click="instanceID = instance.short_id"
                                 class="group border-t last:border-b transition w-full flex flex-row items-center gap-4 border-gray-light py-2">
                                 <div class="type-xs-mono !text-base rounded bg-black py-1 px-3 text-white"
                                     x-text="instance.start_time">
