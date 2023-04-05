11b9258da90b0439f443b6d293e941c02c8b308e
John Puddephatt <john@jdp.org.uk>
2023-04-05 21:28:41 +0100

    don't scroll 2 image galleries on mobile

--- resources/views/vendor/nova-editor-js/carousel.blade.php
+++ resources/views/vendor/nova-editor-js/carousel.blade.php
@@ -1,12 +1,12 @@
-<div class="{{ $width != 'full' ? '-mx-6' : '' }} overflow-hidden my-16 bg-sand-light px-6 pt-6 pb-0">
+<div class="{{ $width != 'full' ? '-mx-6' : '' }} overflow-hidden my-16 bg-sand-light px-4 lg:px-6 pt-6 pb-0">
     <div
-        class="ml-[calc((100vw-100%)/-2)] flex w-screen flex-row gap-5 overflow-x-auto px-[calc((100vw-100%)/2)] scrollbar-hide">
+        class="@if (count($images) < 3) lg:flex-row lg:gap-5 flex-col gap-2 @else flex-row gap-5 @endif ml-[calc((100vw-100%)/-2)] flex w-screen overflow-x-auto px-[calc((100vw-100%)/2)] scrollbar-hide">
         @foreach ($images as $image)
             <figure
-                class="@if (count($images) == 1) w-full @elseif (count($images) == 2) flex-none lg:flex-auto lg:w-auto relative lg:h-screen @else flex-none @endif">
+                class="@if (count($images) < 3) w-full @elseif (count($images) == 2) w-full @else flex-none @endif">
 
                 <img src="{{ $image['url'] }}"
-                    class="@if (count($images) > 2) h-[32em] w-auto @elseif(count($images) == 2) max-w-[80vw] h-[100vw] lg:h-[calc(100%-3rem)] object-cover @else w-full h-auto @endif block rounded">
+                    class="@if (count($images) > 2) h-[32em] w-auto @elseif(count($images) == 2) w-full h-auto lg:max-w-[80vw] lg:h-[calc(100%-3rem)] object-cover @else w-full h-auto @endif block rounded">
 
                 <figcaption class="type-xs-mono py-3">
 
