88a740810f0d10b1a922475a98344d7fac7d6d5a
John Puddephatt <john@jdp.org.uk>
2023-04-05 20:05:47 +0100

    page header alt mobile

--- resources/views/sections/pageheader_alternative.blade.php
+++ resources/views/sections/pageheader_alternative.blade.php
@@ -1,5 +1,5 @@
-<div class="grid grid-cols-2">
-    <div class="bg-sand-light container flex flex-col justify-center">
+<div class="flex flex-col-reverse lg:grid lg:grid-cols-2">
+    <div class="bg-sand-light container flex flex-col justify-center py-6">
         <div class="type-medium lg:type-large max-w-lg pb-12 lg:pb-8">{{ $page->name }}</div>
         <div class="max-w-lg">{{ $page->introduction }}</div>
     </div>
