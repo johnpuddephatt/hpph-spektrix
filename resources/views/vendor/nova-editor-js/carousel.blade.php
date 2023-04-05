2158ad4207bf6566f348372967658925a0c8b7a2
John Puddephatt <john@jdp.org.uk>
2023-04-05 21:32:53 +0100

    figcaption

--- resources/views/vendor/nova-editor-js/carousel.blade.php
+++ resources/views/vendor/nova-editor-js/carousel.blade.php
@@ -8,7 +8,7 @@
                 <img src="{{ $image['url'] }}"
                     class="@if (count($images) > 2) h-[24rem] lg:h-[32em] w-auto @elseif(count($images) == 2) w-full h-auto lg:max-w-[80vw] lg:h-[calc(100%-3rem)] object-cover @else w-full h-auto @endif block rounded">
 
-                @if (isset($image['caption']))
+                @if ($image['caption'])
                     <figcaption class="type-xs-mono pb-6 lg:pb-3 py-3">
 
                         {{ $image['caption'] }}
