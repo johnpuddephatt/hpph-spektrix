02495a1e61560f3f3d177128ff235243a05db90f
John Puddephatt <john@jdp.org.uk>
2023-04-11 19:37:25 +0100

    restrict details/summary width

--- resources/views/vendor/nova-editor-js/detailssummary.blade.php
+++ resources/views/vendor/nova-editor-js/detailssummary.blade.php
@@ -1,4 +1,4 @@
-<details class="">
+<details class="max-w-[50ch]">
     <summary
         class="type-regular flex list-none flex-row gap-4 border-t border-gray-light py-4 focus-within:outline-none hover:bg-gray">
         {!! $summary !!}
