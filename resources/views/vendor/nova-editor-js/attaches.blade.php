a3e26b2f7b3514e711df38079460dc7716600924
John Puddephatt <john@jdp.org.uk>
2023-04-12 13:02:38 +0100

    no underline on attach button

--- resources/views/vendor/nova-editor-js/attaches.blade.php
+++ resources/views/vendor/nova-editor-js/attaches.blade.php
@@ -1,5 +1,5 @@
-<a class="max-w-[50ch] mt-12 mb-5 inline-block rounded bg-yellow p-4" href="{{ Storage::url($file['url']) }}"><span
-        class="type-regular">{{ $title }}</span>&nbsp;
+<a class="max-w-[50ch] mt-12 mb-5 no-underline inline-block rounded bg-yellow p-4"
+    href="{{ Storage::url($file['url']) }}"><span class="type-regular">{{ $title }}</span>&nbsp;
     <span class="type-xs-mono">
         @if ($file['size'] > 1048576)
             [{{ round($file['size'] / 1048576, 1) }}MB]
