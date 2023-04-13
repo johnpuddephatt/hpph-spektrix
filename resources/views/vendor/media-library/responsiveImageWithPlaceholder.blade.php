8dcbfd547dfd9ee62b73ffee18c0ba693007418b
John Puddephatt <john@jdp.org.uk>
2023-04-13 11:56:46 +0100

    try improving image w/placeholder logic to prevent fuzzy images

--- resources/views/vendor/media-library/responsiveImageWithPlaceholder.blade.php
+++ resources/views/vendor/media-library/responsiveImageWithPlaceholder.blade.php
@@ -1,4 +1,18 @@
 <img{!! $attributeString !!} @if ($loadingAttributeValue) loading="{{ $loadingAttributeValue }}" @endif
     srcset="{{ $media->getSrcset($conversion) }}"
-    onload="window.requestAnimationFrame(function(){if(dataset.width) { onload = null; sizes = dataset.width };if(!(size=getBoundingClientRect().width))return;onload=null;sizes=Math.ceil(size/window.innerWidth*100)+'vw';});"
+    onload="window.requestAnimationFrame(function(){
+        if(dataset.width) {
+            console.log('dataset.width:',dataset.width);
+            onload = null; 
+            sizes = dataset.width; 
+        }
+        else if(!(size=getBoundingClientRect().width)) {
+            return;
+        } 
+        else {
+            onload=null;
+            sizes=Math.ceil(size/window.innerWidth*100)+'vw';
+        }
+    })
+        "
     sizes="1px" src="{{ $media->getUrl($conversion) }}" width="{{ $width }}" height="{{ $height }}">
