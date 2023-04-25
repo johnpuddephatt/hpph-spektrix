a4427c84ebd98e3fa1e63a588b219cef1685a2de
John Puddephatt <john@jdp.org.uk>
2023-04-26 00:34:19 +0100

    check

--- resources/views/components/special-event-badge.blade.php
+++ resources/views/components/special-event-badge.blade.php
@@ -1,4 +1,4 @@
-@if ($slot)
+@if ($slot && $slot != '')
     <span
         {{ $attributes->merge(['class' => 'type-xs py-0.5 text-black uppercase inline-block rounded px-2 bg-sand-light !font-bold !no-underline']) }}>With
         {{ $slot }}</span>
