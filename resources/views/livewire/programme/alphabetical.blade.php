5ced4120c95c12e2a356ee8c31a04a5371c41b47
John Puddephatt <john@jdp.org.uk>
2023-04-25 23:49:44 +0100

    reset count on a-z

--- resources/views/livewire/programme/alphabetical.blade.php
+++ resources/views/livewire/programme/alphabetical.blade.php
@@ -1,4 +1,4 @@
-<div class="md:container divide-y divide-gray-light pt-3 pb-8">
+<div x-init="$dispatch('eventcount', { number: null })" class="md:container divide-y divide-gray-light pt-3 pb-8">
     <div class="overflow-hidden">
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 -mx-2 -mb-px">
             @foreach ($events as $event)
