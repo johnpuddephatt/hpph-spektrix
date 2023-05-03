cd4d332ab7f32a471aff232ac0b095c01595c864
John Puddephatt <john@jdp.org.uk>
2023-05-04 00:08:09 +0100

    checkout mobile

--- resources/views/pages/checkout-page.blade.php
+++ resources/views/pages/checkout-page.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.default', ['header_class' => 'text-black', 'edit_link' => route('nova.pages.edit', ['resource' => 'pages', 'resourceId' => $page->id])])
 @section('title', $page->name)
 @section('content')
-    <div class="fixed bg-black -z-10 inset-0 h-[75vh] lg:h-screen lg:w-1/2">
+    <div class="fixed bg-black -z-10 inset-0 h-[50vh] lg:h-screen lg:w-1/2">
 
         @if ($page->mainImage)
             {{ $page->mainImage->img('square')->attributes(['class' => 'h-full w-full object-cover']) }}
         @endif
     </div>
 
-    <div class="mt-[75vh] lg:mt-0 lg:ml-[50%] min-h-screen bg-sand relative">
+    <div class="mt-[50vh] lg:mt-0 lg:ml-[50%] min-h-screen bg-sand relative">
 
-        <div class="bg-sand-light pt-12 pb-0 lg:h-[50vh] flex flex-col">
+        <div class="bg-sand-light pt-12 lg:pt-20 pb-8 lg:h-[50vh] flex flex-col">
 
             <div class="container my-auto max-w-2xl ml-0">
                 <h2 class="type-medium lg:type-large">{{ $page->name }}</h2>
-                <p class="type-regular lg:type-medium !font-normal mt-6">{{ $page->subtitle }}</p>
+                <p class="type-regular lg:type-medium !font-normal lg:mt-6">{{ $page->subtitle }}</p>
             </div>
 
         </div>
