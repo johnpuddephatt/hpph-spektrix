0681b7d29f4d44e45e9eb70f4fd247187dadbb1e
John Puddephatt <john@jdp.org.uk>
2023-05-03 15:34:43 +0100

    constrain overscroll

--- resources/views/pages/checkout-page.blade.php
+++ resources/views/pages/checkout-page.blade.php
@@ -10,22 +10,16 @@
 
     <div class="mt-[75vh] lg:mt-0 lg:ml-[50%] min-h-screen bg-sand relative">
 
-        <div class="bg-sand-light pt-6 pb-12 lg:h-[66.6vh] flex flex-col">
-            <div class="container">
-                <a class="type-xs-mono relative z-50 border-transparent mb-4 inline-block uppercase border-2 pl-1 pr-4 py-2 rounded hover:border-sand"
-                    href="{{ \App\Models\Page::getTemplateUrl('shop-page') }}">@svg('chevron-right', ' align-top h-4 w-4 inline-block transform rotate-180 origin-center')
-                    Back </a>
-            </div>
+        <div class="bg-sand-light pt-12 pb-0 lg:h-[50vh] flex flex-col">
+
             <div class="container my-auto max-w-2xl ml-0">
                 <h2 class="type-medium lg:type-large">{{ $page->name }}</h2>
                 <p class="type-regular lg:type-medium !font-normal mt-6">{{ $page->subtitle }}</p>
             </div>
-            <div class="type-regular pt-12 container !font-normal max-w-2xl ml-0">
-                {{ $page->introduction }}
-            </div>
+
         </div>
 
-        <div class="container pb-24 min-h-screen">
+        <div class="container pt-6 pb-24 min-h-screen">
             <iframe
                 src="https://{{ $settings['spektrix_custom_domain'] }}/{{ $settings['spektrix_client_name'] }}/website/secure/checkout.aspx?resize=true"
                 class="min-h-screen w-[calc(100%-2rem)]" id="SpektrixIFrame" name="SpektrixIFrame"></iframe>
