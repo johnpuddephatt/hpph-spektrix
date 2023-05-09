8ddf9ed551dfdd87d56a984f0db64d9b4666c92d
John Puddephatt <john@jdp.org.uk>
2023-05-09 22:55:03 +0100

    gallery

--- resources/views/vendor/nova-editor-js/carousel.blade.php
+++ resources/views/vendor/nova-editor-js/carousel.blade.php
@@ -1,6 +1,6 @@
 <div class="my-16 relative" x-data="{ timeout: null, scrollPosition: 0, showControls: {{ count($images) > 2 }}, showPreviousControl: true, showNextControl: true }"
     x-effect="$refs.scroller.scrollLeft = scrollPosition; showPreviousControl = !(scrollPosition == 0); showNextControl = !(scrollPosition >= $refs.scroller.scrollWidth - $refs.scroller.clientWidth);">
-    <div class="{{ $width != 'full' ? '-mx-4' : '' }} overflow-hidden bg-sand-light px-4 py-6">
+    <div class="{{ $width != 'full' ? '-mx-4' : '' }} relative overflow-hidden bg-sand-light px-4 py-6">
         <div x-ref="scroller"
             @scroll="clearTimeout(timeout);     timeout = setTimeout(() => {scrollPosition = $el.scrollLeft}, 50)"
             class="@if (count($images) < 3) @else @endif lg:flex-row gap-4 lg:gap-5 flex-col flex-row gap-5 scroll-smooth ml-[calc((100vw-100%)/-2)] flex w-screen overflow-x-auto px-[calc((100vw-100%)/2)] scrollbar-hide">
@@ -9,7 +9,7 @@
                     class="@if (count($images) < 3) w-full @elseif (count($images) == 2) w-full @else flex-none @endif">
 
                     <img src="{{ $image['url'] }}"
-                        class="@if (count($images) > 2) h-[24rem] lg:h-[32em] w-auto @elseif(count($images) == 2) w-full h-auto lg:max-w-[80vw] lg:h-[calc(100%-2rem)] object-cover @else w-full h-auto @endif block rounded">
+                        class="@if (count($images) > 2) h-[20rem] lg:h-[26em] w-auto @elseif(count($images) == 2) w-full h-auto lg:max-w-[80vw] lg:h-[calc(100%-2rem)] object-cover @else w-full h-auto @endif block rounded">
 
                     @if ($image['caption'])
                         <figcaption class="type-xs-mono pt-4">
@@ -19,18 +19,18 @@
                 </figure>
             @endforeach
         </div>
+        <div x-show="showControls" class="text-white absolute bottom-1 right-4">
+            <button :class="{ '!bg-sand-dark': !showPreviousControl }" :disabled="!showPreviousControl"
+                @click="scrollPosition = Math.max(0, scrollPosition - window.innerWidth/1.5)"
+                class="bg-black rounded-full py-1 px-1">
+                @svg('chevron-right', 'rotate-180 h-8 w-8')
+            </button>
+            <button :class="{ '!bg-sand-dark': !showNextControl }" :disabled="!showNextControl"
+                @click="scrollPosition = Math.min($refs.scroller.scrollWidth - $refs.scroller.clientWidth, scrollPosition + window.innerWidth/1.5)"
+                class="bg-black rounded-full py-1 px-1">
+                @svg('chevron-right', 'h-8 w-8')
+            </button>
+        </div>
     </div>
 
-    <div x-show="showControls" class="text-white absolute bottom-1 right-4">
-        <button :class="{ 'opacity-25': !showPreviousControl }" :disabled="!showPreviousControl"
-            @click="scrollPosition = Math.max(0, scrollPosition - window.innerWidth/1.5)"
-            class="bg-black rounded-full py-1 px-1">
-            @svg('chevron-right', 'rotate-180 h-8 w-8')
-        </button>
-        <button :class="{ 'opacity-25': !showNextControl }" :disabled="!showNextControl"
-            @click="scrollPosition = Math.min($refs.scroller.scrollWidth - $refs.scroller.clientWidth, scrollPosition + window.innerWidth/1.5)"
-            class="bg-black rounded-full py-1 px-1">
-            @svg('chevron-right', 'h-8 w-8')
-        </button>
-    </div>
 </div>
