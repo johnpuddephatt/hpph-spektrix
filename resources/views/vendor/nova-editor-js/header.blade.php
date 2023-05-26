045b7c5181e5741f21527a90edde4f44b92ce13b
John Puddephatt <john@jdp.org.uk>
2023-05-26 13:45:42 +0100

    fix heading match

--- resources/views/vendor/nova-editor-js/header.blade.php
+++ resources/views/vendor/nova-editor-js/header.blade.php
@@ -1,3 +1,4 @@
-<{{ "h{$level}" }} class="{{ match ($level) {2 => 'type-medium',3 => 'type-regular'} }} mt-12 mb-8 max-w-[39ch]">
+<{{ "h{$level}" }}
+    class="{{ match ($level) {2 => 'type-medium',3 => 'type-regular',default => 'type-small'} }} mt-12 mb-8 max-w-[39ch]">
     {{ $text }}
     </{{ "h{$level}" }}>
