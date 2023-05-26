b8e749a264e3ebe9fbaa8fadf84d7e9464ef081b
John Puddephatt <john@jdp.org.uk>
2023-05-26 13:55:14 +0100

    fix heading escaping

--- resources/views/vendor/nova-editor-js/header.blade.php
+++ resources/views/vendor/nova-editor-js/header.blade.php
@@ -1,4 +1,4 @@
 <{{ "h{$level}" }}
     class="{{ match ($level) {2 => 'type-medium',3 => 'type-regular',default => 'type-small'} }} mt-12 mb-8 max-w-[39ch]">
-    {{ $text }}
+    {!! $text !!}
     </{{ "h{$level}" }}>
