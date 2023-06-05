73916fbdd82210efba9ae524b0d9510721b9a290
John Puddephatt <john@jdp.org.uk>
2023-06-05 15:41:24 +0100

    account template breakpoint

--- resources/views/pages/account-page.blade.php
+++ resources/views/pages/account-page.blade.php
@@ -4,20 +4,20 @@
 @section('description', $page->seo_description ?? $page->introduction)
 
 @section('content')
-    <div class="fixed bg-black -z-10 inset-0 h-[50vh] md:h-screen md:w-1/2">
+    <div class="fixed bg-black -z-10 inset-0 h-[50vh] xl:h-screen xl:w-1/2">
 
         @if ($page->mainImage)
             {{ $page->mainImage->img('square')->attributes(['class' => 'h-full w-full object-cover']) }}
         @endif
     </div>
 
-    <div class="mt-[50vh] md:mt-0 md:ml-[50%] min-h-screen bg-sand relative">
+    <div class="mt-[50vh] xl:mt-0 xl:ml-[50%] min-h-screen bg-sand relative">
 
-        <div class="bg-sand-light pt-12 md:pt-20 pb-8 md:h-[50vh] flex flex-col">
+        <div class="bg-sand-light pt-12 xl:pt-20 pb-8 xl:h-[50vh] flex flex-col">
 
             <div class="container my-auto max-w-2xl ml-0">
-                <h2 class="type-medium md:type-large">{{ $page->name }}</h2>
-                <p class="type-regular md:type-medium !font-normal md:mt-6">{{ $page->subtitle }}</p>
+                <h2 class="type-medium xl:type-large">{{ $page->name }}</h2>
+                <p class="type-regular xl:type-medium !font-normal xl:mt-6">{{ $page->subtitle }}</p>
             </div>
 
         </div>
@@ -25,7 +25,7 @@
         <div class="container pt-6 pb-24 min-h-screen">
             <iframe
                 src="https://{{ $settings['spektrix_custom_domain'] }}/{{ $settings['spektrix_client_name'] }}/website/secure/myaccount.aspx?resize=true"
-                class="min-h-screen w-full md:w-[calc(100%-2rem)]" id="SpektrixIFrame" name="SpektrixIFrame"></iframe>
+                class="min-h-screen w-full xl:w-[calc(100%-2rem)]" id="SpektrixIFrame" name="SpektrixIFrame"></iframe>
         </div>
 
     </div>
