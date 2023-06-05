da9314d2bc9c8cc50460ee2d7220dfaad3ffdf4f
John Puddephatt <john@jdp.org.uk>
2023-06-05 15:34:37 +0100

    my account breakpoints

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 @if ($settings['alert_enabled'] && $settings['alert_display_until'] > now())
-    <a href="{{ $settings['alert_url'] }}"
+    <a target="_blank" href="{{ $settings['alert_url'] }}"
         class="type-xs-mono flex z-50 bg-yellow text-black lg:max-w-xs lg:w-full pl-1 rounded-full py-1 pr-4 absolute bottom-4 left-4 right-4 lg:bottom-8 lg:left-auto">
 
         @svg('exclamation', 'inline-block rounded-full text-yellow bg-black p-1.5 mr-2 h-6 w-6')
