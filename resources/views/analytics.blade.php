f7d350b28786747d02dd5df5728184d7940aff21
John Puddephatt <john@jdp.org.uk>
2023-06-06 12:33:12 +0100

    analytics

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,12 +1,13 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ settings['google_analytics'] }}"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
+ @if ($settings['google_analytics'])
+     <script async src="https://www.googletagmanager.com/gtag/js?id={{ $settings['google_analytics'] }}"></script>
+     <script>
+         window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+         function gtag() {
+             dataLayer.push(arguments);
+         }
+         gtag('js', new Date());
 
-    gtag('config', "{{ settings['google_analytics'] }}");
-</script>
+         gtag('config', "{{ $settings['google_analytics'] }}");
+     </script>
+ @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,20 +29,6 @@
         </script>
     @endif
 
-    @if ($settings['google_analytics'])
-        <script async src="https://www.googletagmanager.com/gtag/js?id={{ $settings['google_analytics'] }}"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-
-            gtag('config', "{{ $settings['google_analytics'] }}");
-        </script>
-    @endif
-
     @livewireStyles
     @stack('head')
 </head>
