279d76b1bf0731cd783df8283098c828281afc09
John Puddephatt <john@jdp.org.uk>
2023-06-06 11:05:25 +0100

    journal header

--- resources/views/pages/journal-page.blade.php
+++ resources/views/pages/journal-page.blade.php
@@ -4,6 +4,9 @@
 @section('description', $page->seo_description ?? $page->introduction)
 
 @section('content')
-    <x-journal-indexheadercard class="" :post="$page->featured_post" />
+    {{-- <x-journal-indexheadercard class="" :post="$page->featured_post" /> --}}
+    <div class="bg-sand-light pt-36 pb-8">
+        <h1 class="type-medium lg:type-large container">Journal</h1>
+    </div>
     <livewire:posts-index :featured_post="$page->featured_post->id" />
 @endsection
