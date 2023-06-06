fa8bfff774dbffdfbf05e5f07ba0591ff71dc369
John Puddephatt <john@jdp.org.uk>
2023-06-06 11:40:46 +0100

    analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,20 @@
         </script>
     @endif
 
+    @if ($settings['google_analytics'])
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ $settings['google_analytics'] }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', "{{ $settings['google_analytics'] }}");
+        </script>
+    @endif
+
     @livewireStyles
     @stack('head')
 </head>
