4193a1f54360f7674adada5d328ff34f95bae5c2
John Puddephatt <john@jdp.org.uk>
2023-06-06 11:21:09 +0100

    journal cols on 2xl

--- resources/views/components/journal-grid.blade.php
+++ resources/views/components/journal-grid.blade.php
@@ -1,7 +1,7 @@
 @props(['dark' => false, 'posts', 'post_class' => null])
 
 <div
-    {{ $attributes->class('container pt-4 lg:pt-6 grid pb-24 gap-x-6 gap-y-8 lg:gap-y-16 md:grid-cols-2 lg:grid-cols-3') }}>
+    {{ $attributes->class('container pt-4 lg:pt-6 grid pb-24 gap-x-6 gap-y-8 lg:gap-y-16 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4') }}>
     @foreach ($posts as $post)
         <x-journal-card :class="$post_class" :wire:key="$post->id" :post="$post" :dark="$dark" />
     @endforeach
