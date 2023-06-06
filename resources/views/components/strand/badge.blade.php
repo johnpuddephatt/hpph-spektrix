dd4ff12543890d2e416b400f25141f72163dd4bb
John Puddephatt <john@jdp.org.uk>
2023-06-06 10:32:16 +0100

    strand strip on event card

--- resources/views/components/strand/badge.blade.php
+++ resources/views/components/strand/badge.blade.php
@@ -4,7 +4,7 @@
     <div {{ $attributes->class('block text-center uppercase font-bold text-xs text-black whitespace-nowrap rounded py-0.5 px-2') }}
         style="@if ($dark) color:  {{ $strand->color }};  border-width: 1px; border-color:  {{ $strand->color }}; @else background-color: {{ $strand->color }} @endif">
         @if ($partof)
-            <span class="font-normal">Part of</span>
+            <span class="lg:hidden font-normal">Part of</span>
         @endif
         {{ $strand->name }}
     </div>
