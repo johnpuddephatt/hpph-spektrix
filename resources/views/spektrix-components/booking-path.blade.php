668195cca3b2ae59ee463f381ed8c7165392896f
John Puddephatt <john@jdp.org.uk>
2023-06-08 13:12:39 +0100

    booking path squish

--- resources/views/spektrix-components/booking-path.blade.php
+++ resources/views/spektrix-components/booking-path.blade.php
@@ -71,7 +71,7 @@
                                         x-text="instance.start_time">
                                     </div>
 
-                                    <div class="flex items-center flex-row gap-2 flex-wrap">
+                                    <div class="flex items-center flex-row gap-x-2 flex-wrap">
                                         <x-strand.booking-path />
                                         <span
                                             x-show="instance.strand && instance.strand.show_in_booking_path && (instance.captioned || instance.event.audio_description || instance.signed_bsl || instance.relaxed)"
