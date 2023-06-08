626ad29fbf6615384a10700b8a1b80d04d5f52cd
John Puddephatt <john@jdp.org.uk>
2023-06-08 13:04:17 +0100

    part of should be hidden on md

--- resources/views/components/strand/badge.blade.php
+++ resources/views/components/strand/badge.blade.php
@@ -4,7 +4,7 @@
     <div {{ $attributes->class('block text-center uppercase font-bold text-xs text-black whitespace-nowrap rounded py-0.5 px-2') }}
         style="@if ($dark) color:  {{ $strand->color }};  border-width: 1px; border-color:  {{ $strand->color }}; @else background-color: {{ $strand->color }} @endif">
         @if ($partof)
-            <span class="lg:hidden font-normal">Part of</span>
+            <span class="md:hidden font-normal">Part of</span>
         @endif
         {{ $strand->name }}
     </div>
