ce55623e70cbe60846002ec6bc3d949f5e20d2fc
John Puddephatt <john@jdp.org.uk>
2024-01-17 21:19:20 +0000

    remove 'with' from special event tag

--- resources/views/components/accessibilities/booking-path.blade.php
+++ resources/views/components/accessibilities/booking-path.blade.php
@@ -14,5 +14,5 @@
         class="type-xs-mono bg-gray-dark rounded-full text-white block no-underline px-2 text-center cursor-default z-[2]"
         x-show="instance.event.audio_description">AD</abbr>
     <span class="type-xs py-0.5 text-black uppercase inline-block rounded px-2 bg-sand-light !font-bold !no-underline"
-        x-show="instance.special_event" x-text="`with ${instance.special_event}`"></span>
+        x-show="instance.special_event" x-text="instance.special_event"></span>
 </div>
--- resources/views/components/special-event-badge.blade.php
+++ resources/views/components/special-event-badge.blade.php
@@ -1,5 +1,4 @@
 @if ($slot && $slot != '')
     <span
-        {{ $attributes->merge(['class' => 'type-xs py-0.5 text-black uppercase inline-block rounded px-2 bg-sand-light !font-bold !no-underline']) }}>With
-        {{ $slot }}</span>
+        {{ $attributes->merge(['class' => 'type-xs py-0.5 text-black uppercase inline-block rounded px-2 bg-sand-light !font-bold !no-underline']) }}>{{ $slot }}</span>
 @endif
