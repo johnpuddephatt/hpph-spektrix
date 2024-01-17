0fb3cd02574b3bbc29d5238bff718560ee5b321c
John Puddephatt <john@jdp.org.uk>
2024-01-17 23:11:26 +0000

    remove title texT

--- resources/views/spektrix-components/booking-path.blade.php
+++ resources/views/spektrix-components/booking-path.blade.php
@@ -65,7 +65,6 @@
                                     class="type-small mt-12 mb-3" x-text="instance.start_date"></h3>
                                 <button aria-label="Buy tickets for this screening"
                                     x-on:click="instanceID = instance.short_id"
-                                    :title="`${instance.availability} seats available`"
                                     :class="instances[key + 1]?.start_date !== instance.start_date ? 'border-b' : ''"
                                     class="group border-t transition w-full flex flex-row items-center gap-2 lg:gap-4 border-gray-light py-2">
                                     <div class="type-xs-mono !text-base rounded bg-black py-1.5 px-4 text-white"
