1c85affe59adede68b1be906f0b14690c92b2fa9
John Puddephatt <john@jdp.org.uk>
2024-03-06 19:02:00 +0000

    inline releation

--- app/Http/Livewire/NewSearch.php
+++ app/Http/Livewire/NewSearch.php
@@ -16,10 +16,9 @@ class NewSearch extends Component
             strlen($this->search) > 1
                 ? \App\Models\Event::shownInProgramme()
                 ->where("name", "like", "%" . $this->search . "%")
-                ->orWhereHas('instances', function (Builder $query) {
-                    $query->where('season_name', 'like', '%');
-                    // ->orWhere('strand_name', 'like', '%')
-                })
+                ->orWhereRelation('instances', 'strand_name', 'like', '%')
+                // ->orWhere('strand_name', 'like', '%')
+                // })
                 ->with("featuredImage")
                 ->get()
                 : [],
