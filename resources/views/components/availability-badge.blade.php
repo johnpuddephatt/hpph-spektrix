e77ba31bde850c2fc010006ee218047e7b362ed0
John Puddephatt <john@jdp.org.uk>
2024-03-07 19:16:58 +0000

    sold out

--- resources/views/components/availability-badge.blade.php
+++ resources/views/components/availability-badge.blade.php
@@ -7,7 +7,7 @@
         `There are ${instance.availability.seats} regular seats and ${instance.availability.accessible_seats} wheelchair seats available for this screening.` :
         `Regular seating for this screening has sold out. There are ${instance.availability.accessible_seats} wheelchair seats available.`">
 
-    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block opacity-70 w-4 mr-0.5 h-auto" data-name="Layer 1"
+    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block opacity-50 w-4 mr-0.5 h-auto" data-name="Layer 1"
         viewBox="0 0 24 24">
         <path fill="currentColor"
             d="M20 11h-1V5a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v6H4a2 2 0 0 0-2 2v8a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1h10v1a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-8a2 2 0 0 0-2-2ZM7 16a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2H7ZM7 5a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v8.18a3 3 0 0 0-1-.18H8a3 3 0 0 0-1 .18V5Zm-3 8h1v7H4Zm16 7h-1v-7h1Z" />
@@ -15,7 +15,7 @@
     <span x-text="instance.availability.seats > 0  ? '×' + instance.availability.seats : 'Sold out'"></span>
 
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve"
-        width="483.223" height="551.431" class="w-6 border-l opacity-70 border-sand-dark py-1 ml-2 mr-0.5 pl-2 h-auto"
+        width="483.223" height="551.431" class="w-6 border-l opacity-50 border-sand-dark py-1 ml-2 pl-2 h-auto"
         overflow="visible" viewBox="0 0 483.223 551.431">
         <path fill-rule="evenodd" fill="currentColor"
             d="M161.988 98.124c24.963-2.305 44.358-23.811 44.358-48.966C206.346 22.083 184.263 0 157.187 0S108.03 22.083 108.03 49.158c0 8.257 2.304 16.706 6.145 23.81l17.515 246.468 180.397.048 73.991 173.366 97.145-38.098-15.043-35.82-54.367 19.625-71.59-165.28-167.73 1.126-2.303-31.213 121.423.049v-46.183l-126.054-.05-5.57-98.882z"
