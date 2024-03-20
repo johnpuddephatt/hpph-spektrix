972c83641ea2f9c5fd4155910c04f9f80e0bad20
John Puddephatt <john@jdp.org.uk>
2024-03-20 10:49:16 +0000

    event row

--- resources/views/components/event-row.blade.php
+++ resources/views/components/event-row.blade.php
@@ -1,8 +1,11 @@
 @props(['event'])
 
-<div class="group relative pt-4 pb-4 flex flex-row gap-4 lg:gap-6">
+<div class="group relative pt-4 pb-4 flex flex-col md:flex-row gap-4 lg:gap-6">
 
     <div class="w-1/2 md:w-1/4 lg:w-2/12 relative aspect-video flex flex-col">
+        <div class="type-xs-mono max-md:bg-sand-light md:hidden max-md:container max-md:pt-3 pb-3">
+            {!! $event->date_range !!}
+        </div>
         <div class="w-full relative flex-1 bg-gray rounded overflow-hidden">
             @if ($event->featuredImage)
                 {!! $event->featuredImage->img('wide')->attributes(['class' => 'group-hover:scale-105 transition duration-500 absolute inset-0']) !!}
@@ -10,12 +13,12 @@
         </div>
         <x-accessibilities class="absolute top-2 right-1.5" :audiodescribed="$event->audio_description" />
         @if ($event->strand?->show_on_event_card)
-            <x-strand.badge :partof="false" class="mt-2" :strand="$event->strand" />
+            <x-strand.badge :partof="true" class="md:mt-2" :strand="$event->strand" />
         @endif
     </div>
 
     <div class="flex flex-col">
-        <div class="type-xs-mono pb-2 pt-2">
+        <div class="type-xs-mono hidden md:block pb-2 pt-2">
             {!! $event->date_range !!}
         </div>
         <div class="lg:min-h-[4.5rem] mb-4 lg:mb-2">
