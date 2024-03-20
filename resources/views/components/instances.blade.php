c714a1499aad78cb2c94a15092ac9bfc1db4ede0
John Puddephatt <john@jdp.org.uk>
2024-03-20 09:56:54 +0000

    instances date spacing

--- resources/views/components/instances.blade.php
+++ resources/views/components/instances.blade.php
@@ -3,7 +3,7 @@
 <div class="pb-16">
     @forelse ($instances as $instance)
         @if ($loop->index == 0 || $instances->get($loop->index - 1)->start_date !== $instance->start_date)
-            <h3 class="type-xs-mono bg-sand-light container py-4 mt-12 lg:mt-24 first:mt-0">
+            <h3 class="type-xs-mono bg-sand-light container py-3 lg:py-4 mt-12 lg:mt-24 first:mt-0">
                 {{ $instance->start_date }}
             </h3>
         @endif
