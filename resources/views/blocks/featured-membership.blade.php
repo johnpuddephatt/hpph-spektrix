c2f340fa8bb5fdec72bec747e5b6506ca21e6345
John Puddephatt <john@jdp.org.uk>
2024-04-12 17:59:27 +0100

    membership page

--- resources/views/blocks/featured-membership.blade.php
+++ resources/views/blocks/featured-membership.blade.php
@@ -18,27 +18,28 @@
                 class="type-xs-mono absolute left-0 transform text-opacity-50 text-white top-1/2 -rotate-90 origin-center">
                 Member #{{ $settings['15_25_membership_count'] ?? '001' }}</div>
         </div>
+
     </div>
-</div>
 
-<spektrix-memberships class="relative block mx-auto max-w-xs pb-2" client-name="{{ $settings['spektrix_client_name'] }}"
-    custom-domain="{{ $settings['spektrix_custom_domain'] }}" membership-id="{{ $layout->membership->id }}">
+    <spektrix-memberships class="relative block mx-auto max-w-xs pb-2"
+        client-name="{{ $settings['spektrix_client_name'] }}" custom-domain="{{ $settings['spektrix_custom_domain'] }}"
+        membership-id="{{ $layout->membership->id }}">
 
-    <button class="type-regular relative inline-block text-center mt-4 text-yellow" data-submit-membership>
-        @svg('plus', 'mx-auto p-3 mt-12 mb-4 h-12 w-12 rounded-full border text-white border-gray-dark hover:bg-white hover:bg-opacity-25')
-        Add to
-        basket</button>
-    <div class="absolute top-full left-0 right-0" data-success-container style="display: none;">
-        <div class="type-regular max-w-xs text-black leading-tight py-2 px-6 bg-yellow-dark text-center">
-            Added to basket</div>
-        <p class="type-small text-white mt-4">Want your free membership to continue until your 26th birthday? After
-            you’ve processed your order, visit your account page and provide us with your date of birth.</p>
-    </div>
-    <div class="absolute text-black font-bold top-full left-0 right-0 leading-tight py-2 px-6 bg-yellow-dark text-center"
-        data-fail-container style="display: none;">Something went wrong.</div>
-</spektrix-memberships>
+        <button class="type-regular relative inline-block text-center mt-4 text-yellow" data-submit-membership>
+            @svg('plus', 'mx-auto p-3 mt-12 mb-4 h-12 w-12 rounded-full border text-white border-gray-dark hover:bg-white hover:bg-opacity-25')
+            Add to
+            basket</button>
+        <div class="absolute top-full left-0 right-0" data-success-container style="display: none;">
+            <div class="type-regular max-w-xs text-black leading-tight py-2 px-6 bg-yellow-dark text-center">
+                Added to basket</div>
+            <p class="type-small text-white mt-4">Want your free membership to continue until your 26th birthday? After
+                you’ve processed your order, visit your account page and provide us with your date of birth.</p>
+        </div>
+        <div class="absolute text-black font-bold top-full left-0 right-0 leading-tight py-2 px-6 bg-yellow-dark text-center"
+            data-fail-container style="display: none;">Something went wrong.</div>
+    </spektrix-memberships>
 
-<p class="type-xs-mono text-white mt-8">Proof of age will be required when you visit.</p>
+    <p class="type-xs-mono text-white mt-8">Proof of age will be required when you visit.</p>
 
 </div>
 
