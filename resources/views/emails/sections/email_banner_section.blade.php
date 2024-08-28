888961ba8a01820ee75bdefbc688561cd5d1372f
John Puddephatt <john@jdp.org.uk>
2024-08-28 13:56:19 +0100

    padding

--- resources/views/emails/sections/email_banner_section.blade.php
+++ resources/views/emails/sections/email_banner_section.blade.php
@@ -1,6 +1,6 @@
     {{-- prettier-ignore-start --}}
 <mj-wrapper background-color="{{ $section->background ?? '#000000' }}" full-width="full-width">
-<mj-section padding="25px 0">
+<mj-section padding="15px 0">
 
 <mj-image border-radius="5px" href="{{ $section->url }}" src="{{ Storage::disk('digitalocean')->url($section->image) }}" alt="{{ $section->title }}" width="600px" padding="0px"></mj-image>
 </mj-section>
