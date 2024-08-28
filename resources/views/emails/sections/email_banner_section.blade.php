e1aef93d7940cfa366b1828acebcd7e06b541d91
John Puddephatt <john@jdp.org.uk>
2024-08-28 13:55:58 +0100

    border radius

--- resources/views/emails/sections/email_banner_section.blade.php
+++ resources/views/emails/sections/email_banner_section.blade.php
@@ -2,7 +2,7 @@
 <mj-wrapper background-color="{{ $section->background ?? '#000000' }}" full-width="full-width">
 <mj-section padding="25px 0">
 
-<mj-image  href="{{ $section->url }}" src="{{ Storage::disk('digitalocean')->url($section->image) }}" alt="{{ $section->title }}" width="600px" padding="0px"></mj-image>
+<mj-image border-radius="5px" href="{{ $section->url }}" src="{{ Storage::disk('digitalocean')->url($section->image) }}" alt="{{ $section->title }}" width="600px" padding="0px"></mj-image>
 </mj-section>
 </mj-wrapper>
 {{-- prettier-ignore-end --}}
