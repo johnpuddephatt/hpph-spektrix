d9a5fac00f4a491a6d59a3c77508d47dc7b54d51
John Puddephatt <john@jdp.org.uk>
2024-08-28 14:01:54 +0100

    mb

--- resources/views/emails/components/accessibility_icon.blade.php
+++ resources/views/emails/components/accessibility_icon.blade.php
@@ -1,4 +1,4 @@
 <span label="{{ $label }}"
-    style="text-transform: uppercase; margin-bottom: 2px; font-weight: 400; line-height: 1.1;  border-radius: 100px; background-color: #393939; color: #fff; padding: 1px 4px 0;font-size: 10px; display: inline-block">
+    style="text-transform: uppercase; font-weight: 400; line-height: 1.1;  border-radius: 100px; background-color: #393939; color: #fff; padding: 1px 4px 0;font-size: 10px; display: inline-block">
     {{ $abbreviation }}
 </span>
