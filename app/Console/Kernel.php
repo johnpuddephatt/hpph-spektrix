078321f1b2a277f097301920314c7757c3adbf4e
John Puddephatt <john@jdp.org.uk>
2024-10-26 16:51:29 +0100

    availability reinstate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command("fetch:all")->hourly();
         $schedule->command("fetch:shop")->hourly();
-        // $schedule->command("cache:availability")->everyFiveMinutes();
+        $schedule->command("cache:availability")->everyFiveMinutes();
     }
 
     /**
