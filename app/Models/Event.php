02d7ee14a79b3bffb304e5f830631fd2a3bd86d4
John Puddephatt <john@jdp.org.uk>
2025-01-21 14:15:15 +0000

    sort by first not first_date

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -164,7 +164,7 @@ class Event extends Model implements HasMedia, CachableAttributes
 
             ->sortBy([
                 fn($a) => $a->coming_soon ? 1 : 0,
-                fn($a) => $a->allFutureInstances->first()->start_date,
+                fn($a) => $a->allFutureInstances->first()->start,
             ]);
     }
 
