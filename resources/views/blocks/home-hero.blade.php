0859363819b247ede16f9983665edad1ba9b7069
John Puddephatt <john@jdp.org.uk>
2025-01-24 16:29:36 +0000

    reintroduce small delay

--- resources/views/blocks/home-hero.blade.php
+++ resources/views/blocks/home-hero.blade.php
@@ -18,7 +18,7 @@
         @php($event = $layout->event)
 
         {{-- previously had delay-[3500ms] on it, to bring video in after the animation finishes. --}}
-        <div class="inset-0 absolute duration-[2000ms] opacity-0"
+        <div class="inset-0 absolute duration-[2000ms] opacity-0 delay-500"
             x-bind:class="{ '!opacity-100': loaded }" x-data="{ loaded: false }" x-init="loaded = true">
             @if ($event->featuredVideo)
                 {!! $event->featuredVideo->img('thumb', [
