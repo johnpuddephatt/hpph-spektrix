f0cf34ab95a28e7259b0bc29372e2a617f32acd7
John Puddephatt <john@jdp.org.uk>
2025-04-13 23:00:03 +0100

    remove whitespace

--- resources/views/emails/components/key.blade.php
+++ resources/views/emails/components/key.blade.php
@@ -3,34 +3,25 @@
          
 <x-email.heading align="center" type="xs-mono">             
                  Key:  &nbsp;&nbsp;&nbsp;
-
-                  <a style="color: black; text-decoration: none;"
+<a style="color: black; text-decoration: none;"
                      href="https://hpph.co.uk/access#accessible-screenings">@include('emails.components.accessibility_icon', [
                          'label' => 'Captioned',
                          'abbreviation' => 'C',
-                     ]) Captioned</a>
-
-                 &nbsp;&nbsp; <a style="color: black;text-decoration: none;"
+                     ]) Captioned</a>&nbsp;&nbsp;<a style="color: black;text-decoration: none;"
                      href="https://hpph.co.uk/access#accessible-screenings">@include('emails.components.accessibility_icon', [
                          'label' => 'Audio Description',
                          'abbreviation' => 'AD',
                      ])
-                     Audio description</a>
-
-                 &nbsp;&nbsp; <a style="color: black;text-decoration: none;"
+                     Audio description</a>&nbsp;&nbsp; <a style="color: black;text-decoration: none;"
                      href="https://hpph.co.uk/access#accessible-screenings">@include('emails.components.accessibility_icon', [
                          'label' => 'Autism-friendly',
                          'abbreviation' => 'Autism',
-                     ]) Autism-friendly</a>
-
-                         &nbsp;&nbsp; <a style="color: black;text-decoration: none;"
+                     ]) Autism-friendly</a>&nbsp;&nbsp;<a style="color: black;text-decoration: none;"
                      href="https://hpph.co.uk/access#accessible-screenings">@include('emails.components.accessibility_icon', [
                          'label' => 'Toddler-friendly',
                          'abbreviation' => 'Toddler',
                      ])
-                     Toddler-friendly</a>
-
-                        &nbsp;&nbsp; <a style="color: black;text-decoration: none;"
+                     Toddler-friendly</a>&nbsp;&nbsp;<a style="color: black;text-decoration: none;"
                      href="https://hpph.co.uk/strands/bring-your-own-baby"><span title="Bring Your Own Baby"
 style="text-transform: uppercase; line-height: 1.1;  border-radius: 4px; font-weight: 700; background-color: #45cdff;   padding: 1px 4px 0; font-size: 11px; display: inline-block">
 BYOB
