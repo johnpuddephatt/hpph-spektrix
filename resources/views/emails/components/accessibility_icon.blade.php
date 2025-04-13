ed2d28fe49f14fc9f1106e95e2d8b459da31f748
John Puddephatt <john@jdp.org.uk>
2025-04-13 23:02:11 +0100

    access icon weight

--- resources/views/emails/components/accessibility_icon.blade.php
+++ resources/views/emails/components/accessibility_icon.blade.php
@@ -1,4 +1,4 @@
 <span label="{{ $label }}"
-    style="font-family: BasisGrotesque;text-transform: uppercase; vertical-align: middle;  line-height: 1.1;  border-radius: 3px; background-color: #000000; color: #fff; padding: 1px 4px 0;font-size: 10px; display: inline-block">
+    style="font-family: BasisGrotesque;text-transform: uppercase; vertical-align: middle; font-weight: 400; line-height: 1.1;  border-radius: 3px; background-color: #000000; color: #fff; padding: 1px 4px 0;font-size: 10px; display: inline-block">
     {{ $abbreviation }}
 </span>
