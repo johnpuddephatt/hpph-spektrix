d9e0b97d090040a483b27e220bf2a1edc378bdcd
John Puddephatt <john@jdp.org.uk>
2025-04-16 17:40:40 +0100

    border on shop?

--- resources/views/blocks/merchandise-group.blade.php
+++ resources/views/blocks/merchandise-group.blade.php
@@ -1,6 +1,6 @@
 @push('webComponents', '#spektrix-merchandise')
 
-<div class="bg-black py-24 container max-w-7xl px-4 text-white text-center">
+<div class="bg-black border-t border-gray-light py-24 container max-w-7xl px-4 text-white text-center">
     @if($layout->pretitle)
     <p class="type-xs-mono text-center container mb-2">{{ $layout->pretitle }}</p>
     @endif
