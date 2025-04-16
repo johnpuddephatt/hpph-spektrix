2048f19d35590fec22a63e2cd518e3240af6c207
John Puddephatt <john@jdp.org.uk>
2025-04-16 17:45:54 +0100

    typo

--- resources/views/blocks/merchandise-group.blade.php
+++ resources/views/blocks/merchandise-group.blade.php
@@ -31,7 +31,7 @@
                                 Quantity:
                                 <span data-display-quantity></span>   
                             </label>
-                            <button  style="color: @yield('color'); border-color: @yield('color')" class="text-black leading-none type-regular bg-transparent rounded  py-1 px-2.5" data-increment-quantity> + </button>
+                            <button  style="color: @yield('color'); border-color: @yield('color')" class="text-black border leading-none type-regular bg-transparent rounded  py-1 px-2.5" data-increment-quantity> + </button>
                         </div>
                         @endif
                         <button style="background-color: @yield('color')" data-submit-merchandise class="mt-4 w-full border type-regular flex-grow text-black rounded py-3 pl-4 pr-3">Add to basket @svg('arrow-right', 'inline-block h-4 w-4 ml-auto')</button>
