54291bf82cb783cfedc0fc0f6158cf3546f25bb1
John Puddephatt <john@jdp.org.uk>
2025-04-16 17:45:14 +0100

    border on plus/minus

--- resources/views/blocks/merchandise-group.blade.php
+++ resources/views/blocks/merchandise-group.blade.php
@@ -26,15 +26,15 @@
                         @if($layout->display_quantity_controls)
 
                         <div class="flex gap-2 mt-4 justify-center items-center">                    
-                            <button  style="background-color: @yield('color')" class="text-black leading-none  type-regular rounded bg-white py-1 px-2.5" data-decrement-quantity> - </button>
+                            <button  style="color: @yield('color'); border-color: @yield('color')" class="text-black border leading-none  type-regular rounded bg-transparent py-1 px-2.5" data-decrement-quantity> - </button>
                             <label for="quantity">
                                 Quantity:
                                 <span data-display-quantity></span>   
                             </label>
-                            <button  style="background-color: @yield('color')" class="text-black leading-none type-regular rounded bg-white py-1 px-2.5" data-increment-quantity> + </button>
+                            <button  style="color: @yield('color'); border-color: @yield('color')" class="text-black leading-none type-regular bg-transparent rounded  py-1 px-2.5" data-increment-quantity> + </button>
                         </div>
                         @endif
-                        <button style="background-color: @yield('color')" data-submit-merchandise class="mt-4 w-full type-regular flex-grow text-black rounded py-3 pl-4 pr-3">Add to basket @svg('arrow-right', 'inline-block h-4 w-4 ml-auto')</button>
+                        <button style="background-color: @yield('color')" data-submit-merchandise class="mt-4 w-full border type-regular flex-grow text-black rounded py-3 pl-4 pr-3">Add to basket @svg('arrow-right', 'inline-block h-4 w-4 ml-auto')</button>
                     </div>
                     <div class="mt-4 type-regular text-yellow leading-tight py-6 px-6  bg-black flex justify-between"
                         data-success-container style="display: none;"><div>Added to basket</div>
