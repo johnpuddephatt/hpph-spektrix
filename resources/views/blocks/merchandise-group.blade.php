944860bce096ecea72ff54722e1146256a50adce
John Puddephatt <john@jdp.org.uk>
2025-04-16 18:14:08 +0100

    tweaks

--- resources/views/blocks/merchandise-group.blade.php
+++ resources/views/blocks/merchandise-group.blade.php
@@ -39,14 +39,14 @@
                         <button style="background-color: @yield('color')" data-submit-merchandise class="mt-4 w-full border type-regular flex-grow text-black rounded py-3 pl-4 pr-3">Add to basket @svg('arrow-right', 'inline-block h-4 w-4 ml-4')</button>
                         </div>
                     </div>
-                    <div class="absolute left-0 right-0 inset-0 top-full type-regular text-yellow leading-tight py-6 px-6  bg-black "
+                    <div class="absolute rounded left-0 right-0 inset-0 top-full type-regular text-yellow leading-tight py-6 px-6  bg-black "
                         data-success-container style="display: none;">
                         <div class="flex justify-between">
                             <div>Added to basket</div>
                             <a href="/checkout" class="text-white underline">Go to checkout</a> 
                         </div>
                     </div>
-                    <div class="  absolute left-0 inset-0  right-0 top-full text-yellow font-bold leading-tight py-4 px-6 bg-black text-center"
+                    <div class="  absolute rounded left-0 inset-0  right-0 top-full text-yellow font-bold leading-tight py-4 px-6 bg-black text-center"
                         data-fail-container style="display: none;">Something went wrong.</div>
                 </spektrix-merchandise>
 
