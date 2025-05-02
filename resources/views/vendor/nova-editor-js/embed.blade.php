ee7da38043e3dd1dd92fbe8e50d559462b577ee5
John Puddephatt <john@jdp.org.uk>
2025-05-02 20:22:33 +0100

    video embed bug

--- resources/views/vendor/nova-editor-js/embed.blade.php
+++ resources/views/vendor/nova-editor-js/embed.blade.php
@@ -1,4 +1,4 @@
-    <div class="@if ($block_width == 'normal') max-w-2xl @endif relative my-16"
+    <div class="@if ($block_width ?? false == 'normal') max-w-2xl @endif relative my-16"
         style="padding-top: {{ ($height / $width) * 100 }}%">
         <iframe width="{{ $width }}px" height="{{ $height }}px" frameborder="0"
             class="@if ($width !== 'full') rounded @endif absolute inset-0 h-full w-full" allowfullscreen=""
