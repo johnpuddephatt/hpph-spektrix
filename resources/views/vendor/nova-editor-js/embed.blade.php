1951ea0edd312a00bbc476e69c6cd05d65f9ba2c
John Puddephatt <john@jdp.org.uk>
2025-05-02 20:51:07 +0100

    add height/width back in

--- resources/views/vendor/nova-editor-js/embed.blade.php
+++ resources/views/vendor/nova-editor-js/embed.blade.php
@@ -3,7 +3,7 @@ if (!isset($blockwidth)) {
     $blockwidth = '';
 }
 @endphp
-<div class="@if ($blockwidth !== 'full' && $blockwidth !== 'wide') max-w-2xl @endif relative mt-8 mb-12 aspect-video">
+<div style="padding-top: {{ $height/$width * 100}}%" class="@if ($blockwidth !== 'full' && $blockwidth !== 'wide') max-w-2xl @endif relative mt-8 mb-12 ">
     <iframe  frameborder="0"
         class="@if ($blockwidth !== 'full') rounded @endif absolute inset-0 h-full w-full" allowfullscreen=""
         src="{{ $embed }}"></iframe>
