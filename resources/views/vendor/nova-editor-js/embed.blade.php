d6ab7778adb3c4a5c8521168e24c5a3f87cdf9ad
John Puddephatt <john@jdp.org.uk>
2025-05-02 20:46:57 +0100

    blockwidth might not be set?

--- resources/views/vendor/nova-editor-js/embed.blade.php
+++ resources/views/vendor/nova-editor-js/embed.blade.php
@@ -1,10 +1,15 @@
-    <div class="@if ($blockwidth !== 'full' && $blockwidth !== 'wide') max-w-2xl @endif relative mt-8 mb-12 aspect-video">
-        <iframe  frameborder="0"
-            class="@if ($blockwidth !== 'full') rounded @endif absolute inset-0 h-full w-full" allowfullscreen=""
-            src="{{ $embed }}"></iframe>
-            @if($caption)
+@php
+if (!isset($blockwidth)) {
+    $blockwidth = '';
+}
+@endphp
+<div class="@if ($blockwidth !== 'full' && $blockwidth !== 'wide') max-w-2xl @endif relative mt-8 mb-12 aspect-video">
+    <iframe  frameborder="0"
+        class="@if ($blockwidth !== 'full') rounded @endif absolute inset-0 h-full w-full" allowfullscreen=""
+        src="{{ $embed }}"></iframe>
+    @if($caption)
         <div class="caption">
             {{ $caption }}
         </div>
-        @endif
-    </div>
+    @endif
+</div>
