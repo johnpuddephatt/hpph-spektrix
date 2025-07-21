e8599727e63d1b21d135e98f8a158cccc85c4e48
John Puddephatt <john@jdp.org.uk>
2025-07-21 14:41:52 +0100

    max width on funders logo messing with sizing

--- resources/views/seasons/show.blade.php
+++ resources/views/seasons/show.blade.php
@@ -50,8 +50,8 @@
             <div class="prose mt-6 container max-w-3xl text-center">{{ $season->additional_description }}</div>
         @endif
         @if ($season->funders_logo)
-            <img onload="this.style.width = this.clientWidth/2 + 'px'" src="{{ Storage::url($season->funders_logo) }}"
-                alt="" class="px-4 mt-6 mx-auto w-auto h-auto max-w-sm">
+            <img onload="this.style.width = this.clientWidth/2 + 'px'; this.style.maxWidth = '24rem';" src="{{ Storage::url($season->funders_logo) }}"
+                alt="" class="px-4 mt-6 mx-auto w-auto h-auto ">
         @endif
     </div>
 
