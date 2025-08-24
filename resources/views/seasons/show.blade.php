6933132973ac45cdbadd728d7ae204985dacce86
John Puddephatt <john@jdp.org.uk>
2025-08-24 11:10:53 +0100

    opacity

--- resources/views/seasons/show.blade.php
+++ resources/views/seasons/show.blade.php
@@ -6,7 +6,7 @@
 @section('image', $season->featuredImage?->getUrl('landscape'))
 
 {{-- @php($opacity = $season->hpph_presents || $season->show_header ? 'opacity-50' : 'opacity-100') --}}
-@php($opacity = 'opacity-50')
+@php($opacity = 'opacity-60')
 @section('content')
     <div class="fixed inset-0 -z-10 h-[calc(100vh-1rem)] w-full overflow-hidden bg-black">
         @if ($season->featuredVideo && $season->featuredVideo->video_conversions)
