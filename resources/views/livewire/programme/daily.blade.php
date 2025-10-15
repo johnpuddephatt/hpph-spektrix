17667e36aff6cf664e179ea142246a336c53dc67
John Puddephatt <john@jdp.org.uk>
2025-10-15 14:12:04 +0100

    badge on daily

--- resources/views/livewire/programme/daily.blade.php
+++ resources/views/livewire/programme/daily.blade.php
@@ -2,11 +2,11 @@
     @foreach ($instances as $date => $events)
         <div>
             <div id="{{ Str::slug($events->first()->first()->start_date) }}"
-                class="absolute left-0 right-0 h-[3.25rem] bg-sand-light lg:h-0"></div>
+                class="absolute left-0 right-0 h-[4.75rem] bg-sand-light lg:h-0"></div>
             <h3
-                class="{{ $settings['alert_enabled'] && $settings['alert_display_until'] > now() ? 'top-10' : 'top-2' }} container sticky z-10 flex justify-center border-sand py-1 first:mt-0 lg:top-[6.95rem] lg:block lg:border-b lg:bg-sand-light">
+                class="{{ $settings['alert_enabled'] && $settings['alert_display_until'] > now() ? 'top-4' : '-top-5' }} container sticky z-10 flex justify-center border-sand pb-1 pt-8 first:mt-0 lg:top-[6.95rem] lg:block lg:border-b lg:bg-sand-light">
                 <a href="#{{ Str::slug($events->first()->first()->start_date) }}"
-                    class="type-regular inline-block rounded-full bg-yellow px-6 py-2.5">
+                    class="type-small inline-block rounded-full bg-yellow px-6 py-2.5">
                     {{ $events->first()->first()->start_date }}
                 </a>
             </h3>
