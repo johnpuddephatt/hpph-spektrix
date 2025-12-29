0734a5166f4dd8826881268435de004b53b57cbe
John Puddephatt <john@jdp.org.uk>
2025-12-29 15:54:01 +0000

    decrease month calendar

--- resources/views/components/datepicker.blade.php
+++ resources/views/components/datepicker.blade.php
@@ -10,7 +10,7 @@
             <div>
                 <button type="button"
                     class="absolute right-full top-1/2 inline-flex cursor-pointer rounded-full p-1 transition"
-                    @click="month--; getNoOfDays()">
+                    @click="month == 0 ? (month = 11, year--) : month--; getNoOfDays()">
                     <svg class="inline-flex h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
                     </svg>
