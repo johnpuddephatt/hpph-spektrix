9387afecf9d4912474a8af62f0dbb7acdf8f5a43
John Puddephatt <john@jdp.org.uk>
2026-01-03 16:45:03 +0000

    quote size

--- resources/views/vendor/nova-editor-js/quote.blade.php
+++ resources/views/vendor/nova-editor-js/quote.blade.php
@@ -1,6 +1,6 @@
 <blockquote
-    class="{{ $blockwidth == '' ? 'max-w-[50ch]' : '' }} {{ $blockwidth == 'wide' ? 'lg:container lg:!mr-0 lg:w-2/3' : '' }} {{ $blockwidth == 'full' ? 'lg:container lg:ml-[calc(25vw)] lg:text-right lg:w-[75vw] lg:max-w-[calc(55ch+25vw+3rem)]' : '' }} my-16 lg:my-24 relative">
-    <p class="{{ $blockwidth == 'full' ? 'lg:type-large' : '' }} type-medium lg:-indent-3">
+    class="{{ $blockwidth == '' ? 'max-w-[50ch]' : '' }} {{ $blockwidth == 'wide' ? 'lg:container lg:!mr-0 lg:w-2/3' : '' }} {{ $blockwidth == 'full' ? 'lg:container lg:ml-[calc(25vw)] lg:text-right lg:w-[75vw] lg:max-w-[calc(55ch+25vw+3rem)]' : '' }} relative my-16 lg:my-24">
+    <p class="{{ $blockwidth == 'full' ? 'lg:type-large' : 'lg:type-medium' }} type-regular lg:-indent-3">
         “{{ $text }}”
     </p>
     @if ($caption)
