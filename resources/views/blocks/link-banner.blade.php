71e54ec9fd74152c70fe5749af63c697be0da060
John Puddephatt <john@jdp.org.uk>
2026-03-09 17:47:06 +0530

    link banner

--- resources/views/blocks/link-banner.blade.php
+++ resources/views/blocks/link-banner.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ $layout->url }}" class="block border-t-2 border-yellow-dark bg-yellow py-4 transition hover:bg-yellow-dark">
-    <div class="container flex flex-row items-center">
-        <div class="w-1/2">
+    <div class="container flex flex-row items-center gap-4">
+        <div class="lg:w-1/2">
             @svg('plus', 'h-6 w-6')
         </div>
         <div class="type-regular">
--- resources/views/pages/standard-page.blade.php
+++ resources/views/pages/standard-page.blade.php
@@ -8,11 +8,32 @@
 
     @include('sections.pageheader')
 
+    {{--
+    
+    Need to add a title field to all blocks for this to work.
+    Not all blocks need title, but without one they can't appear in nav.
+    We should also use layout->key I think for ID/#anchor.
+    
+    <div class="bg-black py-6 text-white">
+        <div class="container flex flex-row justify-center gap-4">
+            <div class="type-xs-mono">On this page:</div>
+            @foreach ($page->content as $layout)
+                @if ($layout->title)
+                    <a @click="sectionMenuOpen = false; activeSection = section" x-data="{ section: '{{ Illuminate\Support\Str::of($layout->title)->slug() }}' }" class="type-small"
+                        :href="`#${section}`" :class="{ '!bg-black !text-white': activeSection == section }">
+                        {!! $layout->title !!}
+
+                    </a>
+                @endif
+            @endforeach
+        </div>
+    </div> --}}
+
     @if ($page->content)
-        <div x-data="{ open: null}" x-init="open =  window.location.hash.replace('#', '') ">
-        @foreach ($page->content as $layout)
-            @include('blocks.' . $layout->name(), ['layout' => $layout, 'dark' => false])
-        @endforeach
+        <div x-data="{ open: null }" x-init="open = window.location.hash.replace('#', '')">
+            @foreach ($page->content as $layout)
+                @include('blocks.' . $layout->name(), ['layout' => $layout, 'dark' => false])
+            @endforeach
         </div>
     @endif
 @endsection
