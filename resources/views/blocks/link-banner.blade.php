49878e11625032c60160a70f4f31213f396c0c1b
John Puddephatt <john@jdp.org.uk>
2026-03-09 17:55:40 +0530

    layout

--- resources/views/blocks/link-banner.blade.php
+++ resources/views/blocks/link-banner.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ $layout->url }}" class="block border-t-2 border-yellow-dark bg-yellow py-4 transition hover:bg-yellow-dark">
-    <div class="container flex flex-row items-start gap-4">
-        <div class="flex-none lg:w-1/2">
+    <div class="container flex flex-row items-start">
+        <div class="flex-none pr-4 lg:w-1/2">
             @svg('plus', 'h-6 w-6')
         </div>
         <div>
@@ -14,7 +14,7 @@
                 </p>
             @endif
 
-        </div> @svg('arrow-right', 'flex-none ml-auto h-8 w-8 p-2 text-yellow bg-black rounded-full')
+        </div> @svg('arrow-right', 'flex-none ml-auto h-8 w-8 ml-4 p-2 text-yellow bg-black rounded-full')
 
     </div>
 </a>
