743470910dffbcd2c5e99b5c865b1cac8cc7b835
John Puddephatt <john@jdp.org.uk>
2026-03-11 17:25:37 +0530

    bg black

--- resources/views/pages/standard-page.blade.php
+++ resources/views/pages/standard-page.blade.php
@@ -13,9 +13,9 @@
                 return $item instanceof App\Nova\Flexible\Layouts\TextLayout && $item->title;
             })))
 
-        <div class="border-b border-black-light bg-black px-8 text-white" x-data="{ sectionMenuOpen: false, activeSection: null }" x-init="activeSection = window.location.hash.replace('#', '')">
+        <div class="border-b border-black-light bg-black text-white" x-data="{ sectionMenuOpen: false, activeSection: null }" x-init="activeSection = window.location.hash.replace('#', '')">
 
-            <div class="overflow-x-scroll py-6 scrollbar-hide">
+            <div class="overflow-x-scroll px-8 py-6 scrollbar-hide">
                 <div class="justify-center-safe flex flex-row gap-8">
                     <div class="type-xs-mono whitespace-nowrap">Jump to:</div>
                     @foreach ($links as $layout)
