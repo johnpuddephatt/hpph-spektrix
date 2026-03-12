16aabdbe289444a6ba74279f53e0dc095d4e1df6
John Puddephatt <john@jdp.org.uk>
2026-03-12 12:56:10 +0530

    aspect 3 only on larger screens

--- resources/views/blocks/image.blade.php
+++ resources/views/blocks/image.blade.php
@@ -1,7 +1,7 @@
 @if ($layout->image)
     <figure>
         <img src="{{ Storage::url($layout->image) }}"
-            class="{{ $layout->short_height ? 'aspect-[3] object-cover' : '' }} h-auto w-full" />
+            class="{{ $layout->short_height ? 'lg:aspect-[3] object-cover' : '' }} h-auto w-full" />
         @if ($layout->caption)
             <figcaption class="type-xs-mono mt-3">{{ $layout->caption }}</figcaption>
         @endif
