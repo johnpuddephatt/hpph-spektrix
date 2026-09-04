f245411e5367bc500e40de7da53d22582b4f4741
John Puddephatt <john@jdp.org.uk>
2026-09-04 18:03:01 +0100

    build

--- resources/views/blocks/featured-membership.blade.php
+++ resources/views/blocks/featured-membership.blade.php
@@ -25,7 +25,7 @@
     membership-id="{{ $layout->membership->id }}">    
         <button class="type-regular w-full bg-yellow rounded text-center py-4 text-black hover:bg-opacity-90 transition"
         data-submit-membership>Add to basket</button>
-        <div class="-mt-12 relative" data-success-container style="display: none;">
+        <div class="-mt-14 relative" data-success-container style="display: none;">
             <div class="type-regular max-w-lg rounded text-black leading-tight py-4 px-6 bg-yellow-dark text-center">
                 Added to basket</div>
                 <p class="type-small text-white mt-4">Want your free membership to continue until your 26th birthday? After
