ffa0ed595c30b018e299a8a801f847a413f78fd5
John Puddephatt <john@jdp.org.uk>
2026-09-23 17:13:32 +0100

    fotw

--- resources/views/blocks/featured-membership.blade.php
+++ resources/views/blocks/featured-membership.blade.php
@@ -14,15 +14,11 @@
             <ul class="max-w-lg divide-y divide-black-light text-white">
                 @foreach ($layout->membership->benefits as $benefit)
                 <li class="type-xs-mono flex items-center gap-2 py-3 lg:py-4">
-@svg('tick', 'h-6 w-6 p-1 mr-2 rounded-full bg-yellow text-black inline-block')
-
+                    @svg('tick', 'h-6 w-6 p-1 mr-2 rounded-full bg-yellow text-black inline-block')
                     {{ $benefit }}
                 </li>
                 @endforeach
             </ul>
-
-
-
 
             <spektrix-memberships class="relative mt-12 block max-w-lg pb-2"
                 client-name="{{ $settings['spektrix_client_name'] }}"
